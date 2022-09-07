d63ef7de6a4744667a4f94832ffcd0ab2210d4bc
ryan shade <kyunoske@gmail.com>
2022-09-07 10:39:49 +0200

    add smartphoneTest

--- src/main/java/model/Radio.java
+++ src/main/java/model/Radio.java
@@ -1,7 +1,8 @@
 package model;
 
 public interface Radio {
-
+    //All attributes in an interface are public
+    //So you do not need to write public or anything before boolean, String, etc..
     public boolean startRadio();
 
     public boolean stopRadio();
--- src/test/java/model/SmartphoneTest.java
+++ src/test/java/model/SmartphoneTest.java
@@ -9,31 +9,60 @@ class SmartphoneTest {
     @Test
     void startRadio() {
         //Given
+        Friend contact1 = new Friend("Jim", "12345679", "Happy");
+        Friend contact2 = new Friend("Jimmmy", "123456790", "Sad");
+        Friend contact3 = new Friend("Jimmmy Boy", "123456791", "Cheerful");
+        Friend contact4 = new Friend("Jimmmy Boy Wonder", "123456792", "Suprised");
+
+        Friend[] contacts = {contact1, contact2, contact3, contact4};
+
+        Smartphone smartphone = new Smartphone("x", "y", contacts);
+        boolean on = smartphone.startRadio();
 
         //When
+        boolean actual = true;
 
         //Then
+        assertEquals(true, actual);
     }
 
     @Test
     void stopRadio() {
         //Given
+        Friend contact1 = new Friend("Jim", "12345679", "Happy");
+        Friend contact2 = new Friend("Jimmmy", "123456790", "Sad");
+        Friend contact3 = new Friend("Jimmmy Boy", "123456791", "Cheerful");
+        Friend contact4 = new Friend("Jimmmy Boy Wonder", "123456792", "Suprised");
 
+        Friend[] contacts = {contact1, contact2, contact3, contact4};
+
+        Smartphone smartphone = new Smartphone("x", "y", contacts);
+        boolean on = smartphone.stopRadio();
 
         //When
-
+        boolean actual = false;
 
         //Then
+        assertEquals(false, actual);
     }
 
     @Test
     void getPosition() {
         //Given
+        Friend contact1 = new Friend("Jim", "12345679", "Happy");
+        Friend contact2 = new Friend("Jimmmy", "123456790", "Sad");
+        Friend contact3 = new Friend("Jimmmy Boy", "123456791", "Cheerful");
+        Friend contact4 = new Friend("Jimmmy Boy Wonder", "123456792", "Suprised");
 
+        Friend[] contacts = {contact1, contact2, contact3, contact4};
+
+        Smartphone smartphone = new Smartphone("x", "y", contacts);
+        String gps = smartphone.gpsLocation();
 
         //When
-
+        String actual = gps;
 
         //Then
+        assertEquals("Koln", actual);
     }
 }
